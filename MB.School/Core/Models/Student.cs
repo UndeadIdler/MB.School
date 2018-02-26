005d397aa5eebb2ace99b4eedc18717c9b530621
UndeadIdler <421835202@qq.com>
2018-02-26 11:07:39 +0800

    修改用户相关页面

--- MB.School/Core/Models/Student.cs
+++ MB.School/Core/Models/Student.cs
@@ -12,8 +12,10 @@ namespace MB.School.Core.Models
     {
         public int StudentId { get; set; }
 
+        [DisplayName("学生姓名")]
         public string RealName { get; set; }
 
+        [DisplayName("注册时间")]
         public DateTime EnrollmentDate { get; set; }
 
         public ICollection<Enrollment> Enrollments { get; set; }
