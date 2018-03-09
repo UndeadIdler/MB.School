a8279a93342c8b70a1321a9ee39a50a860df4f9b
UndeadIdler <421835202@qq.com>
2018-03-09 15:58:19 +0800

    添加迁移文件

---
+++ MB.School/Application/ViewModels/EnrollmentDateGroup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MB.School.Application.ViewModels
+{
+    public class EnrollmentDateGroup
+    {
+        /// <summary>
+        /// 学生数量
+        /// </summary>
+        [DisplayName("学生数量")]
+        public int StudentCount { get; set; }
+
+        /// <summary>
+        /// 注册日期
+        /// </summary>
+        [DataType(DataType.Date)]
+        [DisplayName("注册日期")]
+        public DateTime? EnrollmentDate { get; set; }
+
+    }
+}
--- MB.School/Controllers/HomeController.cs
+++ MB.School/Controllers/HomeController.cs
@@ -3,23 +3,44 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using MB.School.Application.ViewModels;
+using MB.School.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 using MB.School.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using Remotion.Linq.Clauses;
 
 namespace MB.School.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SchoolDbContext _context;
+
+        public HomeController(SchoolDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
+        public async Task<IActionResult> About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "学生信息统计:";
 
-            return View();
+            var entities = from student in _context.Students
+                group student by student.EnrollmentDate
+                into dataGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dataGroup.Key,
+                    StudentCount =dataGroup.Count()
+                };
+            var dtos =await entities.ToListAsync();
+
+            return View(dtos);
         }
 
         public IActionResult Contact()
